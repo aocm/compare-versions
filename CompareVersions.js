d3d99c8ef809eebd57d07ed4a0e03f035ee99d99
Kazuki Aoshima <aocm.dev@gmail.com>
2020-02-05 23:39:22 +0900

    Class版追加

---
+++ CompareVersions.js
@@ -0,0 +1,12 @@
+export default class CompareVersions{
+  constructor(current, target){
+    this.current = current;
+    this.target = target;
+  }
+  CurrentAndTarget(){
+    return this.current + this.target;
+  }
+  isNewerTarget(){
+    return 
+  }
+}
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,6 +1,15 @@
 import compareVersions from "../index"
+import CompareVersions from "../CompareVersions"
 
 test("sample", ()=>{
   expect(compareVersions("a","b")).toBe("ab")
+
+})
+describe('CompareVersions', () => {
+  const comver = new CompareVersions("a", "b");
+
+  test("AandB", ()=>{
+    expect(comver.CurrentAndTarget()).toBe("ab")
+  })
 
 })
